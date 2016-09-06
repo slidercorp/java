9ce922a9f164df75ed0d4a425195586ebe221ac9
nelson caceres <nelson caceres>
2016-09-06 13:55:26 -0500

    modificio clase conexion

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -10,12 +10,34 @@ package modelo;
  * @author nelson
  */
 public class Conexion {
-    String url;
-    public void setURL(String url){
-        this.url=url;
+    private String url;
+    private String user;
+    private String pass;
+    
+
+
+    public String getUrl() {
+        return url;
     }
-    public String getURL(){
-        return this.url;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
     }
     
 }
