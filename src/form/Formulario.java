68a9bff3a039b03189aba606039170a7519b3c7f
nelson caceres <nelson caceres>
2016-09-06 13:14:51 -0500

    agregue el titulo al formulario

--- src/form/Formulario.java
+++ src/form/Formulario.java
@@ -42,6 +42,7 @@ public class Formulario extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Mostrar Persona");
 
         jLabel1.setText("Mostrar Persona");
 
